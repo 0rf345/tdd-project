7dc7a3fbab2665663524b03ffe89ed81766bdb96
Orfeas Kalipolitis <csd3285@csd.uoc.gr>
2018-04-27 17:27:56 +0300

    Getting the hang of mocking

--- src/functions.js
+++ src/functions.js
@@ -1,12 +1,19 @@
+export function ronSwanson(injectedFetch) {
+  let url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
+  let a;
+  return injectedFetch.get(url).then(res => res.data);
+  return a;
+}
+
 export function add() {
   let sum = 0
   if (arguments.length === 0) return null;
   for (let i in arguments) {
     if (!arguments[i]) return null;
     if (Array.isArray(arguments[i])) {
-      sum += add.apply(this, arguments[i])
+      sum += add.apply(this, arguments[i]);
     } else {
-      sum += arguments[i]
+      sum += arguments[i];
     }
   }
   return sum
--- src/functions.test.js
+++ src/functions.test.js
@@ -1,43 +1,68 @@
 import { add } from './functions';
+import { ronSwanson } from './functions';
+import axios from 'axios';
+
+jest.mock('axios');
+
+describe('Ron Swanson Quotes', () => {
+  let mockAxios = jest.fn();
+  //mockAxios.get.mockImplementation((url) => Promise.resolve({ data: Array('Query')}));
+
+  axios.get.mockImplementation((url) => Promise.resolve({ data: Array('Quote')}));
+
+  it('uses correct url', () => {
+    ronSwanson(axios)
+      .then(res => {
+        expect(axios.get.toBeCalledWith('https://ron-swanson-quotes.herokuapp.com/v2/quotes'));
+      })
+  })
+
+  it('handles correct response', () => {
+    ronSwanson(axios)
+      .then(res => {
+        expect(res[0]).toBe('Quote');
+      })
+  })
+})
 
 describe('Add function', () => {
   it('returns null if there are not enough arguments', () => {
-    expect(add()).toBe(null)
+    expect(add()).toBe(null);
   })
   
   it('returns null if an argument is null', () => {
-    expect(add(null)).toBe(null)
+    expect(add(null)).toBe(null);
   })
   
   it('can perform basic addition', () => {
-    expect(add(1, 1)).toBe(2)
+    expect(add(1, 1)).toBe(2);
   })
 
   describe('Complex addition', () => {
     it('can add positive and negative numbers', () => {
-      expect(add(1, -6)).toBe(-5)
+      expect(add(1, -6)).toBe(-5);
     })
 
     it('can add negative numbers', () => {
-      expect(add(-2, -6)).toBe(-8)
+      expect(add(-2, -6)).toBe(-8);
     })
   })
 
   describe('Array addition', () => {
     it('can add any number of numbers', () => {
-      expect(add(1,2,3)).toBe(6)
+      expect(add(1,2,3)).toBe(6);
     })
     
     it('can add arrays of numbers', () => {
-      expect(add([1,2,3])).toBe(6)
+      expect(add([1,2,3])).toBe(6);
     })
 
     it('can add arrays to other arrays', () => {
-      expect(add([1,2,3], [4,5,6])).toBe(21)
+      expect(add([1,2,3], [4,5,6])).toBe(21);
     })
     
     it('can add arrays of arrays', () => {
-      expect(add([[1,2,3],4], [1,2,3])).toBe(16)
+      expect(add([[1,2,3],4], [1,2,3])).toBe(16);
     })
   })
 })
